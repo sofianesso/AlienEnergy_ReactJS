aa4c85ae765f457845b77bb603816bb97f006f22
sofianesso <114077655+sofianesso@users.noreply.github.com>
2023-03-06 21:45:22 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './app.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
